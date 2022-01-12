ba6145583d85104f5d562cd61db2904122d3238e
JuanPabloGz25 <96797362+JuanPabloGz25@users.noreply.github.com>
2022-01-11 22:16:25 -0500

    feature:some_changes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\StatusController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +23,16 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth','verified'])->name('dashboard');
+})->middleware(['auth','verified','check'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+Route::group(['middleware'=>['auth','verified']],function (){
+    Route::resource('users', UserController::class);
+});
+
+Route::put('/StatusChange/{user}', [StatusController::class,'update'])->name('StatusChange');
+
+Route::get('/disable', [AuthenticatedSessionController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('disable');
+
+    require __DIR__.'/auth.php';
