616479ceae54750e1851b39a73d5edfcd42fac47
JuanPabloGz25 <96797362+JuanPabloGz25@users.noreply.github.com>
2022-02-14 14:00:18 -0500

    feature: make a vitrina controller
    Here, i created a new controller. There is a controller that show the products to the users.

---
+++ app/Http/Controllers/VitrinaController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Products;
+use Illuminate\View\View;
+
+class VitrinaController extends Controller
+{
+    public function vitrina (Request $request): View
+    {
+        $products = Products::where('marca', 'LIKE', '%' . $request->input('search') . '%')
+            ->where('status','enable')->paginate(1)->withQueryString();
+
+        return view('vitrina.index',compact('products'));
+    }
+}
