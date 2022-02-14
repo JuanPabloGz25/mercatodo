ebb34479af8e1ddfed2c81c7c93af128e54435e4
JuanPabloGz25 <96797362+JuanPabloGz25@users.noreply.github.com>
2022-02-14 14:23:04 -0500

    feature: add translates for the logs

---
+++ resources/lang/logs.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title' => 'Registros',
+    'messages' => [
+        'created' => 'Se ha Creado un :model',
+        'updated' => 'Se ha Actualizado un :model',
+        'deleted' => 'Se ha Eliminado un :model'
+    ]
+];
