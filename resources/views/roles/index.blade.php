90d89141db34b709fe1307f4b8688c35986e1ccc
JuanPabloGz25 <96797362+JuanPabloGz25@users.noreply.github.com>
2022-02-17 10:16:41 -0500

    feature: modification of some styles of the view for roles

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -1,41 +1,40 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Tabla de Roles') }}
+            {{ __('TABLA DE ROLES') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="bg-white rounded-lg shadow-sm p-2 text-center flex flex-col gap-3">
-                <a class="btn btn-warning" href="{{ route('roles.create') }}">Crear Nuevo Rol</a>        
-                         
-                         <table class="table table-striped mt-2">
-                           <thead style="background-color:#6777ef">                                     
-                               <th>Nombre del Rol</th>
-                               <th>Acciones</th>                                                          
-                           </thead>
-                           <tbody>
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg text-center">
+
+                <a type="button" href="{{ route('roles.create') }}" class="bg-indigo-500 px-12 py-2 rounded text-black-200 font-semibold hover:bg-indigo-800 transition duration-200 each-in-out">CREAR NUEVO ROL</a>
+                <table class="table-auto w-full">
+                    <thead>
+                    <tr class="bg-gray-800 text-white">
+                        <th class="border px-4 py-2">NOMBRE DEL ROL</th>
+                        <th class="border px-4 py-2">ACCIONES</th>
+                    </tr>
+                    </thead>
+                    <tbody>
                              @foreach ($roles as $rol)
                                <tr>
-                                 <td>{{ $rol->name }}</td>
-                                                              
-                                 <td class="inline-flex">
-                                        <div class="p-2">
-                                            <div class="inline-flex items-center bg-white leading-none text-purple-600 rounded-full p-2 shadow text-teal text-sm">
-                                                <a class="inline-flex bg-teal-300 text-black rounded-full h-6 px-3 justify-center items-center" href="{{ route('roles.edit', $rol->id) }}">{{trans('Editar')}}</a>
-                                            </div>
-                                        </div>
+                                 <td class="border px-4 py-2 border-gray-900">{{ $rol->name }}</td>
 
-                                        <div class="p-2">
-                                            <div class="inline-flex items-center bg-white leading-none text-purple-600 rounded-full p-2 shadow text-teal text-sm">
-                                                {!! Form::open(['method'=>'DELETE', 'route'=>['roles.destroy', $rol->id]]) !!}
-                                                {!! Form::submit(trans('Eliminar')) !!}
-                                                {!! Form::close() !!}
-                                            </div>
-                                        </div>
-                                    </td>
+                                   <td class="border px-4 py-2 border-gray-900">
+                                       <div class="flex justify-center rounded-lg text-lg" role="group">
+                                           <!-- botón editar -->
+                                           <a href="{{ route('roles.edit', $rol->id) }}" class="rounded bg-blue-600 hover:bg-blue-300 text-white font-bold py-2 px-4">EDITAR</a>
+
+                                           <!-- botón borrar -->
+                                           <form action="{{ route('roles.destroy', $rol->id) }}" method="POST" class="formEliminar">
+                                               @csrf
+                                               @method('DELETE')
+                                               <button type="submit" class="rounded bg-red-600 hover:bg-red-300 text-white font-bold py-2 px-4">BORRAR</button>
+                                           </form>
+                                       </div>
+                                   </td>
                                </tr>
                              @endforeach
                            </tbody>
@@ -49,3 +48,31 @@
         </div>
     </div>
 </x-app-layout>
+<script>
+    (function () {
+        'use strict'
+        //debemos crear la clase formEliminar dentro del form del boton borrar
+        //recordar que cada registro a eliminar esta contenido en un form
+        var forms = document.querySelectorAll('.formEliminar')
+        Array.prototype.slice.call(forms)
+            .forEach(function (form) {
+                form.addEventListener('submit', function (event) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                    Swal.fire({
+                        title: '¿CONFIRMA LA ELIMINACIÓN DEL ROL?',
+                        icon: 'info',
+                        showCancelButton: true,
+                        confirmButtonColor: '#20c997',
+                        cancelButtonColor: '#6c757d',
+                        confirmButtonText: 'CONFIRMAR'
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            this.submit();
+                            Swal.fire('¡ELIMINADO!', 'EL ROL HA SIDO ELIMINADO EXITOSAMENTE.','success');
+                        }
+                    })
+                }, false)
+            })
+    })()
+</script>
