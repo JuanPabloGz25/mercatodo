dbf012454673ecaecd36efcf415bb39c61586790
JuanPabloGz25 <96797362+JuanPabloGz25@users.noreply.github.com>
2022-03-30 17:51:13 -0500

    feature: modification the views for the authentication

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,63 @@
 <x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital@1&display=swap" rel="stylesheet">
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+    <div class="bg-gradient-to-r from-gray-900 via-gray-700 to-gray-900 py-6">
+        <h1 class="text-3xl text-center text-white uppercase" style="font-family: 'Merriweather', cursive;">Inicia Sesión En Nuestra Web</h1>
+    </div>
+    <div class="flex justify-between py-8 px-6 bg-gradient-to-r from-blue-900 via-blue-400 to-blue-900">
+        <div>
+            <img src="{{ asset('image/landcruiser.png') }}" alt="landcruiser" class="w-100 h-100 px-20">
+        </div>
+        <div class="px-24 py-10">
+            <!-- Session Status -->
+            <x-auth-session-status class="mb-4" :status="session('status')" />
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+            <!-- Validation Errors -->
+            <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-        <form method="POST" action="{{ route('login') }}">
+            <form method="POST" action="{{ route('login') }}">
             @csrf
 
             <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
+                <div>
+                    <x-label for="email" :value="__('CORREO ELECTRÓNICO')" />
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
+                    <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                </div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+                <!-- Password -->
+                <div class="mt-10">
+                    <x-label for="password" :value="__('CONTRASEÑA')" />
 
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
+                    <x-input id="password" class="block mt-1 w-full"
+                             type="password"
+                             name="password"
+                             required autocomplete="current-password" />
+                </div>
 
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+                <!-- Remember Me -->
+                <div class="block mt-8">
+                    <label for="remember_me" class="inline-flex items-center">
+                        <input id="remember_me" type="checkbox" class="mt-4 rounded-full border-black border-2 text-red-600 shadow-sm focus:border-red-400 focus:ring focus:ring-red-200 focus:ring-opacity-50" name="remember">
+                        <span class="mt-4 ml-4 text-sm text-black font-semibold">{{ __('RECUÉRDAME') }}</span>
+                    </label>
+                </div>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
+                <div class="flex items-center justify-end mt-10">
+                    @if (Route::has('password.request'))
+                        <a class="underline text-sm text-white font-semibold hover:text-black" href="{{ route('password.request') }}">
+                            {{ __('¿OLVIDASTE TU CONTRASEÑA?') }}
+                        </a>
+                    @endif
 
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
+                    <x-button class="ml-3">
+                        {{ __('INICIA SESIÓN') }}
+                    </x-button>
+                </div>
+            </form>
+        </div>
+
+    </div>
 </x-guest-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,59 +1,98 @@
 <x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital@1&display=swap" rel="stylesheet">
 
+    <div class="bg-gradient-to-r from-gray-900 via-gray-700 to-gray-900 py-4">
+        <h1 class="text-3xl text-center text-white uppercase" style="font-family: 'Merriweather', cursive;">Regístrate en Nuestra Web</h1>
+    </div>
+
+    <div class="flex justify-around bg-gradient-to-r from-blue-900 via-blue-400 to-blue-900 px-10 py-4">
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
         <form method="POST" action="{{ route('register') }}">
             @csrf
+        <div>
+                <!-- Validation Errors -->
+                <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-            <!-- Name -->
-            <div>
-                <x-label for="name" :value="__('Name')" />
+                <form method="POST" action="{{ route('register') }}">
+                @csrf
+                <!-- Name -->
+                <div>
+                    <x-label for="name" :value="__('NOMBRE')" />
 
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
-            </div>
+                    <x-input id="name" class="block mt-1 w-80 h-8" type="text" name="name" :value="old('name')" required autofocus />
+                </div>
 
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
+                <!-- Lastname -->
+                <div>
+                    <x-label for="lastname" :value="__('APELLIDOS')" />
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
+                    <x-input id="lastname" class="block mt-1 w-80 h-8" type="text" name="lastname" :value="old('lastname')" required autofocus />
+                </div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+                <!-- Email Address -->
+                <div>
+                    <x-label for="email" :value="__('CORREO ELECTRÓNICO')" />
 
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
-            </div>
+                    <x-input id="email" class="block mt-1 w-80 h-8" type="email" name="email" :value="old('email')" required />
+                </div>
 
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
+                <!-- Document -->
+                <div>
+                    <x-label for="document" :value="__('IDENTIFICACIÓN')" />
 
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-            </div>
+                    <x-input id="document" class="block mt-1 w-80 h-8" type="text" name="document" :value="old('document')" required autofocus />
+                </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
+                <!-- Phone -->
+                <div>
+                    <x-label for="phone" :value="__('NÚMERO DE CONTACTO')" />
 
-                <x-button class="ml-4">
-                    {{ __('Register') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
+                    <x-input id="phone" class="block mt-1 w-80 h-8" type="text" name="phone" :value="old('phone')" required autofocus />
+                </div>
+
+                <!-- Gender -->
+                <div>
+                    <x-label for="gender" :value="__('GÉNERO')" />
+
+                    <x-input id="gender" class="block mt-1 w-80 h-8" type="text" name="gender" :value="old('gender')" required autofocus />
+                </div>
+
+                <!-- Password -->
+                <div>
+                    <x-label for="password" :value="__('CONTRASEÑA')" />
+
+                    <x-input id="password" class="block mt-1 w-80 h-8"
+                         type="password"
+                         name="password"
+                         required autocomplete="new-password" />
+                </div>
+
+                <!-- Confirm Password -->
+                <div>
+                    <x-label for="password_confirmation" :value="__('CONFIRMAR CONTRASEÑA')" />
+
+                    <x-input id="password_confirmation" class="block mt-1 w-80 h-8"
+                         type="password"
+                         name="password_confirmation" required />
+                </div>
+        </div>
+                <div class="flex items-center justify-center mt-4">
+                    <a class="underline text-sm text-white font-semibold hover:text-black" href="{{ route('login') }}">
+                        {{ __('¿YA ESTÁS REGISTRADO?') }}
+                    </a>
+
+                    <x-button class="ml-4">
+                        {{ __('REGÍSTRESE') }}
+                    </x-button>
+                </div>
+                </form>
+        <div class="flex justify-between">
+            <img src="{{ asset('image/opcion1.avif') }}" alt="opcion1" class="rounded-3xl mr-4 border-2 border-white">
+            <img src="{{ asset('image/opcion2.avif') }}" alt="opcion2" class="rounded-3xl border-2 border-white">
+        </div>
+    </div>
 </x-guest-layout>
