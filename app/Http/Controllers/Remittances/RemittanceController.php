45879cd22ea7f4d7eb02d369c08e8d2572802763
JuanPabloGz25 <96797362+JuanPabloGz25@users.noreply.github.com>
2022-03-30 17:59:01 -0500

    feature: creation of the methods for the remittances

---
+++ app/Http/Controllers/Remittances/RemittanceController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Remittances;
+
+use App\Contracts\RemittanceGatewayContract;
+use App\Http\Controllers\Controller;
+use App\Models\Remittances\Remittance;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use function auth;
+use function redirect;
+use function view;
+
+class RemittanceController extends Controller
+{
+    public function index(RemittanceGatewayContract $RemittanceGatewayContract): View
+    {
+        $user = auth()->user()->id;
+        $remittances = Remittance::where('user_id', $user)->paginate(6);
+        foreach ($remittances as $remittance)
+        {
+            $RemittanceGatewayContract->queryRemittance($remittance);
+        }
+
+        return view('invoices.index', compact('remittances'));
+    }
+
+    public function store(Request $request, RemittanceGatewayContract $remittanceGatewayContract): RedirectResponse
+    {
+        $shoppingCart = \Cart::getContent();
+        $data = $remittanceGatewayContract->createSession($shoppingCart, $request);
+//        dd($data->response);
+        return $data->payment->status == 'pending'
+            ? redirect($data->response->processUrl()): redirect()->route('catalogo');
+    }
+
+    public function show($id): View
+    {
+        $products = \Cart::getContent();
+        $remittance = Remittance::find($id);
+        return view('invoices.show',compact('remittance','products'));
+    }
+
+    public function update(Request $request, $id): void
+    {
+        //
+    }
+}
