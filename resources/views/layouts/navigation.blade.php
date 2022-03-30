9c4ff619994782dde41eeba020cabd75d49f199f
JuanPabloGz25 <96797362+JuanPabloGz25@users.noreply.github.com>
2022-03-30 17:49:07 -0500

    feature: modification of layouts views

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,49 +1,49 @@
-<nav x-data="{ open: false }" class="bg-blue-50 border-b border-blue-400">
+<nav x-data="{ open: false }" class="bg-gray-800 border-b-2 border-red-700">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-black-600" />
-                    </a>
-                </div>
-
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Inicio') }}
+                        {{ __('INICIO') }}
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('catalogo')" :active="request()->routeIs('catalogo')">
-                        {{ __('Catálogo') }}
+                        <img src="https://cdn4.iconfinder.com/data/icons/LUMINA/transportation/png/256/muscle_car.png" alt="" class="w-12 h-12">
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')">
-                        {{ __('Usuarios') }}
+                    <x-nav-link :href="route('actualidad')" :active="request()->routeIs('actualidad')">
+                        <img src="https://cdn3.iconfinder.com/data/icons/eziconic-v1-0/256/02.png" alt="" class="w-12 h-12">
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('products.index')" :active="request()->routeIs('products.index')">
-                        {{ __('Productos') }}
+                    <x-nav-link :href="route('cart.store')" :active="request()->routeIs('cart.store')">
+                       <img src="https://cdn4.iconfinder.com/data/icons/e-commerce/empty-shopping-cart.png" alt="" class="w-10 h-10">
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('roles.index')" :active="request()->routeIs('roles.index')">
-                        {{ __('Roles') }}
+                    <x-nav-link :href="route('external-api.index')" :active="request()->routeIs('external-api.index')">
+                        <img src="https://cdn0.iconfinder.com/data/icons/ie_Financial_set/256/08.png" alt="" class="w-10 h-10">
                     </x-nav-link>
                 </div>
+                @can('ver-historial')
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('pagos')" :active="request()->routeIs('pagos')">
+                        <img src="https://cdn4.iconfinder.com/data/icons/free-large-boss-icon-set/256/Admin.png" alt="" class="w-10 h-10">
+                    </x-nav-link>
+                </div>
+                @endcan
             </div>
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                            <div>{{ Auth::user()->name }}</div>
+                        <button class="flex items-center text-sm font-medium text-yellow-400 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                            <div class="uppercase font-bold text-yellow-400">{{ Auth::user()->name }}</div>
 
                             <div class="ml-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
@@ -55,57 +55,61 @@
 
                     <x-slot name="content">
                         <!-- Authentication -->
+                        @can('ver-usuarios')
+                        <form method="GET" action="{{ route('users.index') }}">
+                            @csrf
+
+                            <x-dropdown-link :href="route('roles.index')"
+                                             onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                {{ __('Gestión de Usuarios') }}
+                            </x-dropdown-link>
+                        </form>
+                        @endcan
+                        @can('ver-productos')
+                        <form method="GET" action="{{ route('products.index') }}">
+                            @csrf
+
+                            <x-dropdown-link :href="route('roles.index')"
+                                             onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                {{ __('Gestión de Productos') }}
+                            </x-dropdown-link>
+                        </form>
+                        @endcan
+                        @can('ver-noticias')
+                        <form method="GET" action="{{ route('news.index') }}">
+                            @csrf
+
+                            <x-dropdown-link :href="route('roles.index')"
+                                             onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                {{ __('Gestión de Noticias') }}
+                            </x-dropdown-link>
+                        </form>
+                        @endcan
+                        @can('ver-roles')
+                        <form method="GET" action="{{ route('roles.index') }}">
+                            @csrf
+
+                            <x-dropdown-link :href="route('roles.index')"
+                                             onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                {{ __('Gestión de Roles') }}
+                            </x-dropdown-link>
+                        </form>
+                        @endcan
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
 
                             <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
+                                             onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                {{ __('Log Out') }}
+                                {{ __('Cerrar Sesión') }}
                             </x-dropdown-link>
                         </form>
                     </x-slot>
                 </x-dropdown>
-            </div>
-
-            <!-- Hamburger -->
-            <div class="-mr-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
-        </div>
-    </div>
-
-    <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div>
-
-        <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
-            <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-            </div>
-
-            <div class="mt-3 space-y-1">
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form>
             </div>
         </div>
     </div>
