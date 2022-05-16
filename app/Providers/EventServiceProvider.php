49c37846fb5076fee42f3fd7917a9aed94fbcca9
Juan Pablo <juanpablo.7721@gmail.com>
2022-05-16 01:27:59 -0500

    feat: adding the new visit in providers

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Events\NewVisited;
+use App\Listeners\AddNewVisit;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -24,6 +26,10 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+
+        NewVisited::class => [
+            AddNewVisit::class,
+        ]
     ];
 
     /**
